f2b8493267f2be1d77e02bb07b2abb5d406ff5e9
Yogurt7v <egorov-2k@yandex.ru>
2024-06-05 12:36:10 +0300

    layouts

---
+++ src/app/_lib/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header style={{ backgroundColor: 'blue', color: 'white', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <p style={{fontSize: '30px'}}>Header</p>
+        </header>
+    )
+}
