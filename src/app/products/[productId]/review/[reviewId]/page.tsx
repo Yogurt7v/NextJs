a08666cc030af37837ba92330498292b7a647769
Yogurt7v <egorov-2k@yandex.ru>
2024-06-06 12:26:23 +0300

    error message

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,6 +1,16 @@
 import NotFound from "@/app/not-found"
 
+function getRandomInt (count: number) {
+    return Math.floor(Math.random() * count)
+}
+
 function ReviewWithId ({params} : {params: {productId: string, reviewId: string}}) {
+    const random = getRandomInt(2)
+
+    if(random ===1 ){
+        throw new Error('Error loading')
+    }
+
     if(parseInt(params.reviewId) >=100) {
         return (
             <NotFound />
@@ -13,3 +23,4 @@ function ReviewWithId ({params} : {params: {productId: string, reviewId: string}
 }
 
 export default ReviewWithId
+
